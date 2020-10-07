492dadab44f500494761c9f8978db147aea36805
Techmetix Solutions Pvt Ltd <techmetix.in@gmail.com>
2020-10-07 11:11:27 +0530

    otp with login api

---
+++ app/src/main/java/com/techmetix/realtimeinvestment/cont.java
@@ -0,0 +1,9 @@
+package com.techmetix.realtimeinvestment;
+
+public class cont {
+
+    private static final String baseurl = "https://admin.realtimeinvestment.in/api/v1/";
+
+    public static final  String url_sendotp = baseurl+"sendotp/";
+
+}
